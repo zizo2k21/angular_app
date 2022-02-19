2eaee091b629c59a42a6939436e851a29adc92a2
zizo2k21 <yedjaziz@yahoo.fr>
2022-02-19 16:11:37 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ColDef } from 'ag-grid-community';
+
+
+import champion from './_files/champion.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my_app';
+
+ /* columnDefs: ColDef=[
+    {headerName: "Make", field:"make"},
+    {headerName: "Model", field:"model"},
+    {headerName: "Price", field:"price"}
+  ]; 
+  
+  rowData=[
+    {make: 'Toyota', model: 'Corrola', price:35000},
+    {make: 'Ford', model: 'Mondeo', price:32000},
+    {make: 'Porche', model: 'Boxter', price:72000},
+  ]*/
 }
